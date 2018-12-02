a1b9359e0b84f01afd9977bc490a3b58f97c9a43
Thomas Lent <tcl293@gmail.com>
2018-12-02 13:57:08 -0500

    Remove unnecessary result returned from main

--- solutions/day-2/src/main.rs
+++ solutions/day-2/src/main.rs
@@ -3,13 +3,11 @@ extern crate day_2;
 pub const INPUT: &'static str = include_str!("../input");
 
 use day_2::{calculate_checksum, find_similar_id_match, parse_input};
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let ids = parse_input(INPUT);
     let result = calculate_checksum(&ids);
     println!("{}", result);
     let result = find_similar_id_match(&ids);
     println!("{}", result);
-    Ok(())
 }
