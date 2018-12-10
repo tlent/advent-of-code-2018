9b3e3a0471772e73d84fd41ad61ce1f16d0f1152
Thomas Lent <tcl293@gmail.com>
2018-12-10 16:47:01 -0500

    Day 10

---
+++ solutions/day-10/src/main.rs
@@ -0,0 +1,11 @@
+use day_10::{parse_input, solve};
+use std::error::Error;
+
+const INPUT: &str = include_str!("../input");
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let points = parse_input(INPUT)?;
+  let solution = solve(&points);
+  println!("{}{}", solution.0, solution.1);
+  Ok(())
+}
