4833201494d274b0098d9cf00956a35d44d58243
Thomas Lent <tcl293@gmail.com>
2018-12-11 17:51:59 -0500

    Add day 11 benchmark

---
+++ solutions/day-11/benches/bench.rs
@@ -0,0 +1,14 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use day_11::solve;
+
+const INPUT: u32 = 5034;
+const FUEL_CELL_GRID_SIZE: usize = 300;
+
+fn criterion_benchmark(c: &mut Criterion) {
+  c.bench_function("solve part two", |b| {
+    b.iter(|| solve(FUEL_CELL_GRID_SIZE, INPUT, 1, 299))
+  });
+}
+
+criterion_group! {name = benches; config = Criterion::default().sample_size(20); targets = criterion_benchmark}
+criterion_main!(benches);
