1a0b29c6f797d79d007c4cdde273123daaece3cd
Thomas Lent <tcl293@gmail.com>
2018-12-11 17:42:38 -0500

    Blazing fast day 11

--- solutions/day-11/src/main.rs
+++ solutions/day-11/src/main.rs
@@ -1,93 +1,80 @@
-use std::collections::HashMap;
-
 const INPUT: u32 = 5034;
-const FUEL_CELL_GRID_SIZE: u32 = 300;
-const PART_ONE_SQUARE_SIZE: u32 = 3;
-
-fn calculate_power_level(coordinate: (u32, u32), serial_number: u32) -> i32 {
-  let rack_id = coordinate.0 + 10;
-  (((rack_id * coordinate.1 + serial_number) * rack_id / 100) % 10) as i32 - 5
-}
-
-fn calculate_square_power_level(
-  coordinate: (u32, u32),
-  serial_number: u32,
-  square_size: u32,
-) -> i32 {
-  let mut power_level = 0;
-  for y in coordinate.1..coordinate.1 + square_size {
-    for x in coordinate.0..coordinate.0 + square_size {
-      power_level += calculate_power_level((x, y), serial_number);
-    }
-  }
-  power_level
-}
-
-fn solve_part_one(serial_number: u32) -> (u32, u32) {
-  solve(serial_number, &[PART_ONE_SQUARE_SIZE]).0
-}
-
-fn extend_square_power_level(
-  previous_square_power_level: i32,
-  coordinate: (u32, u32),
-  serial_number: u32,
-  new_size: u32,
-) -> i32 {
-  let mut power_level = previous_square_power_level;
-  for x in coordinate.0..(coordinate.0 + new_size) {
-    let y = coordinate.1 + new_size - 1;
-    power_level += calculate_power_level((x, y), serial_number);
-  }
-  // subtract 1 to avoid counting bottom right corner twice
-  for y in coordinate.1..(coordinate.1 + new_size - 1) {
-    let x = coordinate.0 + new_size - 1;
-    power_level += calculate_power_level((x, y), serial_number);
-  }
-  power_level
-}
-
-fn solve(serial_number: u32, square_sizes: &[u32]) -> ((u32, u32), u32) {
-  let mut previous_square_power_levels = HashMap::new();
-  square_sizes
-    .iter()
-    .map(|&square_size| {
-      let mut possible_coordinates = vec![];
-      for start_y in 1..=(FUEL_CELL_GRID_SIZE - square_size) {
-        for start_x in 1..=(FUEL_CELL_GRID_SIZE - square_size) {
-          possible_coordinates.push((start_x, start_y));
-        }
-      }
-      let (max_power_level, c) = possible_coordinates
-        .iter()
-        .map(|&c| {
-          let power_level = match previous_square_power_levels.get(&(c, square_size - 1)) {
-            Some(&previous_power_level) => {
-              extend_square_power_level(previous_power_level, c, serial_number, square_size)
-            }
-            None => calculate_square_power_level(c, serial_number, square_size),
-          };
-          previous_square_power_levels.insert((c, square_size), power_level);
-          (power_level, c)
-        })
-        .max_by_key(|&(power_level, ..)| power_level)
-        .unwrap();
-      (max_power_level, c, square_size)
-    })
-    .max_by_key(|&(power_level, ..)| power_level)
-    .map(|(_power_level, c, square_size)| (c, square_size))
-    .unwrap()
-}
-
-fn solve_part_two(serial_number: u32) -> ((u32, u32), u32) {
-  let square_sizes: Vec<_> = (1..FUEL_CELL_GRID_SIZE).collect();
-  solve(serial_number, &square_sizes)
-}
+const FUEL_CELL_GRID_SIZE: usize = 300;
+const PART_ONE_SQUARE_SIZE: usize = 3;
 
 fn main() {
   let part_one_solution = solve_part_one(INPUT);
   println!("{},{}", part_one_solution.0, part_one_solution.1);
   let (c, size) = solve_part_two(INPUT);
   println!("{},{},{}", c.0, c.1, size);
+}
+
+fn solve_part_one(serial_number: u32) -> (usize, usize) {
+  solve(serial_number, PART_ONE_SQUARE_SIZE, PART_ONE_SQUARE_SIZE).0
+}
+
+fn solve_part_two(serial_number: u32) -> ((usize, usize), usize) {
+  solve(serial_number, 1, FUEL_CELL_GRID_SIZE - 1)
+}
+
+fn calculate_power_level(coordinate: (u32, u32), serial_number: u32) -> i32 {
+  let rack_id = coordinate.0 + 10;
+  (((rack_id * coordinate.1 + serial_number) * rack_id / 100) % 10) as i32 - 5
+}
+
+fn solve(
+  serial_number: u32,
+  min_square_size: usize,
+  max_square_size: usize,
+) -> ((usize, usize), usize) {
+  if max_square_size >= FUEL_CELL_GRID_SIZE {
+    panic!("max square size must be less than grid size");
+  }
+  let power_levels: Vec<Vec<_>> = (1..=FUEL_CELL_GRID_SIZE as u32)
+    .map(|y| {
+      (1..=FUEL_CELL_GRID_SIZE as u32)
+        .map(|x| calculate_power_level((x, y), serial_number))
+        .collect()
+    })
+    .collect();
+  let transposed_power_levels: Vec<Vec<_>> = (0..power_levels[0].len())
+    .map(|x| {
+      (0..power_levels.len())
+        .map(|y| power_levels[y][x])
+        .collect()
+    })
+    .collect();
+  let mut results = Vec::with_capacity(max_square_size - min_square_size + 1);
+  let mut square_power_levels = power_levels.clone();
+  for square_size in 1..=max_square_size {
+    let max_index = FUEL_CELL_GRID_SIZE - square_size;
+    let mut coordinates = Vec::with_capacity(max_index * max_index);
+    (0..max_index).for_each(|i| (0..max_index).for_each(|j| coordinates.push((i, j))));
+    if square_size >= min_square_size {
+      let max_result = coordinates
+        .iter()
+        .map(|&(i, j)| {
+          let y = i + 1;
+          let x = j + 1;
+          (square_power_levels[i][j], (x, y), square_size)
+        })
+        .max_by_key(|&(power_level, ..)| power_level)
+        .unwrap();
+      results.push(max_result);
+    }
+    coordinates.iter().for_each(|&(i, j)| {
+      let max_i = i + square_size;
+      let max_j = j + square_size;
+      // only the row range is inclusive to avoid counting the bottom right cell twice
+      square_power_levels[i][j] += power_levels[max_i][j..=max_j].iter().sum::<i32>()
+        + transposed_power_levels[max_j][i..max_i].iter().sum::<i32>();
+    });
+  }
+  results
+    .into_iter()
+    .max_by_key(|&(power_level, ..)| power_level)
+    .map(|(_power_level, coordinate, square_size)| (coordinate, square_size))
+    .unwrap()
 }
 
 #[cfg(test)]
@@ -119,6 +106,7 @@ mod tests {
   }
 
   #[test]
+  #[ignore]
   fn it_solves_part_two_correctly() {
     assert_eq!(
       solve_part_two(PART_TWO_SAMPLE_SERIAL_NUMBERS[0]),
