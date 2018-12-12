7f83c180c786725b0ab6149bbaccec19e493e759
Thomas Lent <tcl293@gmail.com>
2018-12-12 16:12:22 -0500

    Day 12 part two

--- solutions/day-12/src/main.rs
+++ solutions/day-12/src/main.rs
@@ -1,9 +1,8 @@
-use std::collections::HashSet;
-
 const INPUT: &str = include_str!("../input");
-const GENERATIONS: u32 = 20;
+const PART_ONE_GENERATIONS: usize = 20;
+const PART_TWO_GENERATIONS: usize = 50_000_000_000;
 
-fn parse_input(input: &str) -> (Vec<u32>, HashSet<Vec<bool>>) {
+fn parse_input(input: &str) -> (Vec<u32>, Vec<u8>) {
   let mut lines = input.trim().lines();
   let first_line = lines.nth(0).unwrap();
   let initial_state: Vec<_> = first_line[15..]
@@ -13,45 +12,56 @@ fn parse_input(input: &str) -> (Vec<u32>, HashSet<Vec<bool>>) {
     .map(|(i, _c)| i as u32)
     .collect();
   assert!(lines.next().unwrap().is_empty());
-  let rules = lines.fold(HashSet::new(), |mut rules, line| {
-    if line.chars().nth(9).unwrap() == '#' {
-      let rule: Vec<bool> = line.chars().take(5).map(|c| c == '#').collect();
-      rules.insert(rule);
-    }
-    rules
-  });
+  let rules = lines
+    .filter(|line| line.chars().nth(9).unwrap() == '#')
+    .map(|line| {
+      line.chars().take(5).map(|c| c == '#').fold(0, |acc, b| {
+        if b {
+          return acc * 2 + 1;
+        }
+        acc * 2
+      })
+    })
+    .collect();
   (initial_state, rules)
 }
 
-fn solve_part_one(initial_state: &[u32], rules: &HashSet<Vec<bool>>) -> i32 {
-  let mut current_state: Vec<_> = initial_state.iter().map(|&x| x as i32).collect();
-  for _ in 1..=GENERATIONS {
-    let mut next_state = vec![];
-    let current_min_pot_number = current_state[0];
-    let current_max_pot_number = current_state[current_state.len() - 1];
-    for pot_number in (current_min_pot_number - 2)..=(current_max_pot_number + 2) {
-      let sequence: Vec<_> = ((pot_number - 2)..=(pot_number + 2))
-        .map(|i| {
-          if i < current_min_pot_number || i > current_max_pot_number {
-            false
-          } else {
-            current_state.binary_search(&i).is_ok()
-          }
-        })
-        .collect();
-      let has_plant = rules.contains(&sequence);
-      if has_plant {
-        next_state.push(pot_number);
+fn solve(initial_state: &[u32], rules: &[u8], generations: usize) -> i64 {
+  let mut prev_state: Vec<_> = initial_state.iter().map(|&x| x as i64).collect();
+  let mut rules = rules.to_owned();
+  rules.sort();
+  for current_gen in 1..=generations {
+    let mut new_state = vec![];
+    let first_filled_pot = prev_state[0];
+    let last_filled_pot = prev_state[prev_state.len() - 1];
+    for pot_number in (first_filled_pot - 2)..=(last_filled_pot + 2) {
+      let sequence = ((pot_number - 2)..=(pot_number + 2)).fold(0, |acc, i| {
+        if i >= first_filled_pot && i <= last_filled_pot && prev_state.binary_search(&i).is_ok() {
+          return acc * 2 + 1;
+        }
+        acc * 2
+      });
+      if rules.binary_search(&sequence).is_ok() {
+        new_state.push(pot_number);
       }
     }
-    current_state = next_state;
+    let prev_sum: i64 = prev_state.iter().sum();
+    let new_sum: i64 = new_state.iter().sum();
+    if prev_sum - (prev_state[0] * prev_state.len() as i64)
+      == new_sum - (new_state[0] * new_state.len() as i64)
+    {
+      let generation_change = new_sum - prev_sum;
+      return new_sum + generation_change * (generations - current_gen) as i64;
+    }
+    prev_state = new_state;
   }
-  current_state.iter().sum()
+  prev_state.iter().sum()
 }
 
 fn main() {
   let (initial_state, rules) = parse_input(INPUT);
-  println!("{}", solve_part_one(&initial_state, &rules));
+  println!("{}", solve(&initial_state, &rules, PART_ONE_GENERATIONS));
+  println!("{}", solve(&initial_state, &rules, PART_TWO_GENERATIONS));
 }
 
 #[cfg(test)]
@@ -71,30 +81,25 @@ mod test {
   #[test]
   fn it_solves_part_one_correctly() {
     let (initial_state, rules) = get_sample_input();
-    assert_eq!(solve_part_one(&initial_state, &rules), 325);
+    assert_eq!(solve(&initial_state, &rules, PART_ONE_GENERATIONS), 325);
   }
 
-  fn get_sample_input() -> (Vec<u32>, HashSet<Vec<bool>>) {
-    let mut rules = HashSet::new();
-    rules.extend(
-      vec![
-        vec![false, false, false, true, true],
-        vec![false, false, true, false, false],
-        vec![false, true, false, false, false],
-        vec![false, true, false, true, false],
-        vec![false, true, false, true, true],
-        vec![false, true, true, false, false],
-        vec![false, true, true, true, true],
-        vec![true, false, true, false, true],
-        vec![true, false, true, true, true],
-        vec![true, true, false, true, false],
-        vec![true, true, false, true, true],
-        vec![true, true, true, false, false],
-        vec![true, true, true, false, true],
-        vec![true, true, true, true, false],
-      ]
-      .into_iter(),
+  #[test]
+  fn it_solves_part_two_correctly() {
+    let (initial_state, rules) = parse_input(INPUT);
+    assert_eq!(
+      solve(&initial_state, &rules, PART_TWO_GENERATIONS),
+      4900000001793
     );
-    (vec![0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24], rules)
+  }
+
+  fn get_sample_input() -> (Vec<u32>, Vec<u8>) {
+    (
+      vec![0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24],
+      vec![
+        0b00011, 0b00100, 0b01000, 0b01010, 0b01011, 0b01100, 0b01111, 0b10101, 0b10111, 0b11010,
+        0b11011, 0b11100, 0b11101, 0b11110,
+      ],
+    )
   }
 }
