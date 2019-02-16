c5df70c1c92c503414b00a352a9ecbf27458bde7
Thomas Lent <tcl293@gmail.com>
2019-02-15 20:02:16 -0500

    Clean up day 1 solution

--- solutions/day-1/src/main.rs
+++ solutions/day-1/src/main.rs
@@ -1,16 +1,57 @@
-extern crate day_1;
-extern crate failure;
+use std::collections::HashSet;
 
-use day_1::{find_repeat_result, parse_input, sum};
-use failure::Error;
+const INPUT: &str = include_str!("../input");
 
-pub const INPUT: &'static str = include_str!("../input");
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<()> {
     let numbers = parse_input(INPUT)?;
-    let result = sum(&numbers);
-    println!("{}", result);
-    let result = find_repeat_result(&numbers);
-    println!("{}", result);
+    println!("{}", sum(&numbers));
+    println!("{}", find_repeat_result(&numbers));
     Ok(())
+}
+
+fn parse_input(input: &str) -> Result<Vec<i32>> {
+    input
+        .split_whitespace()
+        .map(|x| x.parse().map_err(Box::from))
+        .collect()
+}
+
+fn sum(numbers: &[i32]) -> i32 {
+    numbers.iter().sum()
+}
+
+pub fn find_repeat_result(numbers: &[i32]) -> i32 {
+    let mut past_results = HashSet::new();
+    let mut result = 0;
+    past_results.insert(result);
+    loop {
+        for number in numbers {
+            result += number;
+            let is_repeat = !past_results.insert(result);
+            if is_repeat {
+                return result;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sum_returns_correct_result() {
+        assert_eq!(sum(&[2, 2]), 4);
+        assert_eq!(sum(&[2, 2, -4, 0, 1]), 1);
+    }
+
+    #[test]
+    fn find_repeat_result_returns_correct_result() {
+        assert_eq!(find_repeat_result(&[1, -1]), 0);
+        assert_eq!(find_repeat_result(&[3, 3, 4, -2, -4]), 10);
+        assert_eq!(find_repeat_result(&[-6, 3, 8, 5, -6]), 5);
+        assert_eq!(find_repeat_result(&[7, 7, -2, -7, -4]), 14);
+    }
 }
